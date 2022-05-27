484f4b93f81740c91bdcdc8ff9fefe763dddb17c
Vicente Almeida <vicentecostaalmeida@gmail.com>
2022-05-27 16:32:54 +0200

    implement api routes for drafts table

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/prompts", async (req, res) => {
       "INSERT INTO prompts (body) VALUES ($1) RETURNING *",
       [prompt]
     );
-    res.json(response.rows);
+    res.json(response.rows[0]);
   } catch (err) {
     console.log(err.detail);
   }
@@ -40,7 +40,7 @@ app.put("/prompts/:id", async (req, res) => {
       "UPDATE prompts SET body = $1 WHERE id = $2 RETURNING *",
       [new_body, id]
     );
-    res.json(response.rows);
+    res.json(response.rows[0]);
   } catch (err) {
     console.log(err);
   }
@@ -54,13 +54,61 @@ app.delete("/prompts/:id", async (req, res) => {
       "DELETE FROM prompts WHERE id = $1 RETURNING *",
       [id]
     );
-    res.json(response.rows);
+    res.json(response.rows[0]);
   } catch (err) {
     console.log(err);
   }
 });
 
 // get drafts
+app.get("/drafts", async (req, res) => {
+  try {
+    const drafts = await pool.query("SELECT * FROM drafts");
+    res.json(drafts.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // create draft
+app.post("/drafts", async (req, res) => {
+  try {
+    const { datetime_created, title, prompt_used, body } = req.body;
+    const response = await pool.query(
+      "INSERT INTO drafts (datetime_created, title, prompt_used, body) VALUES ($1, $2, $3, $4) RETURNING *",
+      [datetime_created, title, prompt_used, body]
+    );
+    res.json(response.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // make changes to existing draft
+app.put("/drafts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, body } = req.body;
+    const response = await pool.query(
+      "UPDATE drafts SET title = $1, body = $2 WHERE id = $3 RETURNING *",
+      [title, body, id]
+    );
+    res.json(response.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // delete draft
+app.delete("/drafts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await pool.query(
+      "DELETE FROM drafts WHERE id = $1 RETURNING *",
+      [id]
+    );
+    res.json(response.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
